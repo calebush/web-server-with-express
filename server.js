096b1a752708324e263e90b4d84e307f187581ac
bushendich <bushendichcaleb@gmail.com>
2020-12-31 13:00:20 +0300

    intial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+var app = express();
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public'))
+
+app.use((req,res,next)=>{
+var now = new Date().toString();
+var log = `${now}: ${req.method} ${req.url}`
+fs.appendFile('server.log',log + '\n', (err)=>{
+    if(err){
+        console.log("unable to log to the server")
+    }
+})
+console.log(log)
+next();
+})
+
+// app.use((req, res, next)=>{
+//     res.render('maintenance.hbs',{
+//         pageTitle:'Website Under Maintenance',
+//         currentYear: new Date().getFullYear(),
+//     })
+// })
+
+app.get('/', (req, res)=>{
+res.render('home.hbs',{
+    pageTitle:'My Node Website',
+    currentYear: new Date().getFullYear(),
+})
+})
+
+app.get('/about',(req, res)=>{
+    res.render('about.hbs', {
+        pageTitle:'Welcome About Page',
+        currentYear: new Date().getFullYear(),
+    })
+})
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage:"Page Not found"
+    })
+})
+
+app.listen(5000)
